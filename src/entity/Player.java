bbe0e1e062facc4980030f794d14832169e4ffe4
Adryan Lima <acals@poli.br>
2025-08-18 23:10:21 -0300

    Add: animações concluídas e sprites adicionados

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -23,7 +23,14 @@ public class Player extends Entity {
     
     public void getPlayerImage() {
     	try {
-    		standing = ImageIO.read(getClass().getResourceAsStream("/player/standing.png"));
+    	    down1 = ImageIO.read(getClass().getResourceAsStream("/player/down1.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/down2.png"));
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/up1.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/up2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/left1.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/left2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/right1.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/right2.png"));
     	}catch(IOException e) {
     		e.printStackTrace();
     	}
@@ -32,34 +39,76 @@ public class Player extends Entity {
         x = 100;
         y = 100;
         speed = 4;
-        direction = "standing";
+        direction = "down";
     }
     public void update(){
-        if (keyH.upPressed == true){
-        	direction = "standing";//provisorio so pra ver a funcionalidade dos sprites
-            y -= speed;
-        }
-        else if (keyH.downPressed == true){
-        	direction = "standing";
-            y += speed;
-        }
-        else if (keyH.leftPressed == true){
-        	direction = "standing";
-            x -= speed;
-        }
-        else if (keyH.rightPressed == true){
-        	direction = "standing";
-            x += speed;
+        if(keyH.downPressed==true || keyH.leftPressed == true || keyH.upPressed == true || keyH.rightPressed == true){
+            if (keyH.upPressed == true){
+                direction = "up";
+                y -= speed;
+            }
+            else if (keyH.downPressed == true){
+                direction = "down";
+                y += speed;
+            }
+            else if (keyH.leftPressed == true){
+                direction = "left";
+                x -= speed;
+            }
+            else if (keyH.rightPressed == true){
+                direction = "right";
+                x += speed;
+            }
+            spriteCounter++;
+            if(spriteCounter>12){
+                if(spriteNum==1){
+                    spriteNum=2;
+                } else if (spriteNum==2) {
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+
         }
 
+        }
     }
     public void draw(Graphics2D g2){
         BufferedImage image = null;
-        
+        //perdão pela identação, a IDE ta bugada
         switch(direction) {
-        case "standing":
-        	image = standing;
+        case "up":
+            if(spriteNum == 1){
+                image = up1;
+            }
+            if(spriteNum == 2){
+                image = up2;
+            }
+
         	break;
+            case "down":
+                if(spriteNum == 1){
+                    image = down1;
+                }
+                if(spriteNum == 2){
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1){
+                    image = left1;
+                }
+                if(spriteNum == 2){
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1){
+                    image = right1;
+                }
+                if(spriteNum == 2){
+                    image = right2;
+                }
+                break;
         }
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
         
